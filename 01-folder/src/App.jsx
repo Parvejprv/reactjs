ea123b9f82e3a22f06a8db00a80977279b0c6420
Parvej Alam <parvejprv1@gmail.com>
2026-01-17 23:58:48 +0530

    01-folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,20 @@
+
+const App = () => {
+  return (
+    // Fragment syntax
+    <>
+      <div className="dad" id="dad">
+        <h1 id="dad-h1">Dad</h1>
+        <p id="dad-p">This is the Dad component.</p>
+        <h3 id="dad-h3">Hope you are doing well!</h3>
+      </div>
+      <div className="mom" id="mom">
+        <h1 id="mom-h1">Mom</h1>
+        <p id="mom-p">This is the Mom component.</p>
+        <h3 id="mom-h3">Have a great day!</h3>
+      </div>
+    </>
+  );
+};
+
+export default App;
